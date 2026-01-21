c8078ec73b5d3967169d65ed6c984024acf051b1
Nithusan <nithusak@ifi.uio.no>
2026-01-21 20:41:24 +0100

    Initial Commit

---
+++ MatLogg/MatLoggApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatLoggApp.swift
+//  MatLogg
+//
+//  Created by Nithusan Krishnasamymudali on 21/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MatLoggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MatLoggTests/MatLoggTests.swift
@@ -0,0 +1,17 @@
+//
+//  MatLoggTests.swift
+//  MatLoggTests
+//
+//  Created by Nithusan Krishnasamymudali on 21/01/2026.
+//
+
+import Testing
+@testable import MatLogg
+
+struct MatLoggTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
